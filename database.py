77583471d3355449bfecb66055ef96a68dc804ce
Trenton Potter <trpotter72@gmail.com>
2022-02-21 21:37:09 -0600

    Fix port conflict, Procfile, and yarn things I don't understand

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = create_engine(
-    'postgresql://alex@localhost:5432/crm', convert_unicode=True)
+    'postgresql://potter@localhost:5432/crm', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
