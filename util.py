5803b68674be7e152980c6b9e7c96f5aa40d42f0
Harrison Luo <harryl6798@gmail.com>
2022-02-25 22:52:40 -0800

    Added the email functionality to populate with friends

--- util.py
+++ util.py
@@ -1,9 +1,10 @@
 from base64 import b64decode
 
+
 def localize_id(id):
     """
     graphql ids look something like: RnJpZW5kOjE=
-    
+
     To get this, we take the type of object it is (Friend) and the ID in the
     database (1), and put them together:
     'Friend:1'
@@ -20,3 +21,24 @@ def localize_id(id):
         return b64decode(id).decode('utf-8').split(':')[1]
     except:
         raise Exception('Could not parse local id from string: ' + id)
+
+
+ADD_FRIEND = "{index}. {friend_name} \n"
+
+
+def format_email(users_name, list_of_friends):
+    file = open('email_templates/default_email_template.txt', 'r')
+    email_template = file.read()
+    friends = ""
+    for index, friend in enumerate(list_of_friends):
+        friends += ADD_FRIEND.format(index=index+1, friend_name=friend)
+
+    # Add all the info to the email template
+    email_template = email_template.format(
+        users_name=users_name, friends=friends)
+
+    file.close()
+    return email_template
+
+
+print(format_email("Alex", ["Harry", "Trent"]))
