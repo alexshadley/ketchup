d7b49d5275fdad3a3216cd790fecd37040fd8fd5
Alex Shadley <shadleyalex@gmail.com>
2022-03-05 09:51:13 -0800

    adds a (very) basic script to send emails

---
+++ email_worker.py
@@ -0,0 +1,22 @@
+from database import db_session
+from emails import send_simple_message
+from models import Friend, User
+from util import format_email
+
+def send_emails():
+    """
+    Super basic, sends an email to every user telling them to message all of
+    their friends. Once we get more certainty around how we want to handle
+    frequencies we can make this less dumb.
+    """
+    users = User.query.all()
+
+    for user in users:
+        friends = Friend.query.filter_by(user_email=user.email).all()
+
+        message = format_email(user.email, [f.name for f in friends])
+        send_simple_message(user.email, 'Ketchup Time', message)
+
+if __name__ == '__main__':
+    send_emails()
+
